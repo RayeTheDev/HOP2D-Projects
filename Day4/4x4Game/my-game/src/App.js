f86c3be011628d1014eae5f3bba8d2d215c39011
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2022-10-21 22:18:36 +0800

    my-game

--- Day4/4x4Game/my-game/src/App.js
+++ Day4/4x4Game/my-game/src/App.js
@@ -3,17 +3,48 @@ import "./App.css";
 import { useState } from "react";
 
 const Box = (props) => {
-  return <div className="box"></div>;
+
+  if (props.index == props.insert[0]) {
+    return <div className="Insert"></div>
+  } else {
+    return <div className="box"></div>;
+  }
 };
 
+
+
 function App() {
+  const [insert, setInsert] = useState(0)
   const jaja = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   const [arr, setArr] = useState(jaja);
+
+  function moveKeys(event, props) {
+    console.log(event.key)
+    console.log(insert)
+    if (event.key == "a" && insert > 0) {
+      setInsert(insert - 1)
+    }
+
+    if (event.key == "d") {
+      setInsert(insert + 1)
+    }
+    if (event.key == "w" && insert > 1) {
+
+      setInsert(insert - 4)
+    }
+    if (event.key == "s") {
+      setInsert(insert + 4)
+    }
+  }
+
+  document.onkeydown = moveKeys
+
+
   return (
     <div className="App">
       <div class="boxCont">
         {arr.map((box, index) => (
-          <Box index={index}></Box>
+          <Box index={index} insert={[insert, setInsert]}></Box>
         ))}
       </div>
     </div>
