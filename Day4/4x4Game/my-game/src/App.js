3318d7b398638d54a00c93e71cb1513e6c16f7c2
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2022-10-25 13:49:56 +0800

    dsad

--- Day4/4x4Game/my-game/src/App.js
+++ Day4/4x4Game/my-game/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Box = (props) => {
   if (props.index == props.insert[0]) {
@@ -11,50 +11,62 @@ const Box = (props) => {
 };
 
 function App() {
+
   const [insert, setInsert] = useState(0);
-   const value = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-  const [data, setData] = useState([{ value: value[Math.floor(Math.random() * value.length)], id: "", isSelected: false }]);
-  const jaja = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
- 
+  const jaja = [1, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
   const [arr, setArr] = useState(jaja);
 
 
+  useEffect(() => {
+    function shuffle() {
+      setArr(prev => {
 
+        const arr = [...prev]
+        let currentIndex = arr.length, randomIndex;
 
-  // function shuffle(value) {
-  //   let currentIndex = value.length,  randomIndex;
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
 
-  //   // While there remain elements to shuffle.
-  //   while (currentIndex != 0) {
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
 
-  //     // Pick a remaining element.
-  //     randomIndex = Math.floor(Math.random() * currentIndex);
-  //     currentIndex--;
+          // And swap it with the current element.
+          [arr[currentIndex], arr[randomIndex]] = [
+            arr[randomIndex], arr[currentIndex]];
+        }
 
-  //     // And swap it with the current element.
-  //     [value[currentIndex], value[randomIndex]] = [
-  //       value[randomIndex], value[currentIndex]];
-  //   }
+        return arr;
+      })
+    }
 
-  //   return value;
-  // }
-  // shuffle()
+    shuffle()
+  }, [])
+
+  // arr.map((arr2El, index) => {
+  //   arr2El = num[parseInt(Math.random() * num.length)];
+  //   let numIndex = num.indexOf(arr2El);
+  //   num.splice(numIndex, 1);
+  //   arr[index] = arr2El;
+  // });
 
   function moveKeys(event) {
     console.log(event.key);
-    console.log(insert);
-    if (event.key == "a" && insert > 0) {
+    if (event.key == "a" && insert % 4 !== 0) {
       setInsert(insert - 1);
     }
-
-    if (event.key == "d") {
+    if (event.key == "d" && (insert !== 3 && insert !== 7 && insert !== 11 && insert !== 15)) {
       setInsert(insert + 1);
     }
-    if (event.key == "w" && insert > 1) {
+    if (event.key == "w" && insert >= 4) {
       setInsert(insert - 4);
     }
-    if (event.key == "s") {
+    if (event.key == "s" && insert < 12) {
       setInsert(insert + 4);
+    }
+
+    if (event.key == "Space") {
+
     }
   }
 
@@ -64,7 +76,7 @@ function App() {
     <div className="App">
       <div class="boxCont">
         {arr.map((box, index) => (
-          <Box value={value} index={index} insert={[insert, setInsert]}></Box>
+          <Box value={box} index={index} insert={[insert, setInsert]}></Box>
         ))}
       </div>
     </div>
@@ -72,3 +84,4 @@ function App() {
 }
 
 export default App;
+
