a1654b250f00c37340adf830ceae27aaa34a5bf2
Geto1-creator <gongor.namba@gmail.com>
2022-12-20 19:15:02 +0800

    ip

--- MyMusic/music-app/src/components/CreateList.js
+++ MyMusic/music-app/src/components/CreateList.js
@@ -1,13 +1,42 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../App'
-import styles from './assets/createlist.module.css'
+import { useContext } from "react";
+import { Button } from "react-bootstrap";
+import { ThemeContext } from "../App";
+import styles from "./assets/createlist.module.css";
 
 export const CreateList = () => {
-    const { create, setCreate } = useContext(ThemeContext)
-    console.log(create)
-    return (
-        <div className={create ? styles.container : `${styles.container} ${styles.displayNone}`}>
-            <span>HELLOW</span>
+  const { create, setCreate } = useContext(ThemeContext);
+  console.log(create);
+  return (
+    <div
+      className={
+        create ? styles.container : `${styles.container} ${styles.displayNone}`
+      }
+    >
+      <div className={styles.headSector}>
+        <span className={styles.title}>Create Playlist</span>
+        <span>Exit</span>
+      </div>
+      <div className={styles.midSector}>
+        <button variant="light" className={styles.imgInp}></button>
+        <div className={styles.infoSector}>
+          <div>
+            <span className={styles.infoText}>Name:</span>
+            <input placeholder="My Playlist #1" className={styles.inp}></input>
+          </div>
+          <div>
+            <span className={styles.infoText}>Description:</span>
+            <input placeholder="Add an optional description"  className={styles.inpD}></input>
+          </div>
+          <Button className={styles.saveButton}variant="light" >Create</Button>
         </div>
-    )
-}
+
+      </div>
+      <div className={styles.botSector}>
+        <span className={styles.botText}>
+        By proceeding, you agree to give Invader access to the image you choose to upload. Please make sure you have the right to upload the image.
+        </span>
+      
+      </div>
+    </div>
+  );
+};
--- MyMusic/music-app/src/components/Pages/Home.js
+++ MyMusic/music-app/src/components/Pages/Home.js
@@ -10,43 +10,44 @@ import { CreateList } from "../CreateList";
 import { ThemeContext } from "../../App";
 
 export const Home = () => {
-
   const [data, setData] = useState([]);
   const { create, setCreate } = useContext(ThemeContext);
 
-
   return (
-    <div className={styles.Container}>
-      <CreateList />
-      <div className={styles.topCont}>
-        {/* <img className={styles.coverImg} src={Cover}></img>s */}
-        <div className={styles.coverTextCont}>
-          <span className={styles.coverTitle}>Connect on Invader</span>
-          <p className={styles.coverText}>
-            Discover, stream, and share a constantly expanding mix of music from
-            emerging and major artists around the world.
-          </p>
-          {/* <Button className={styles.coverButton}>Sign up for free</Button> */}
+    <>
+      <div className={ styles.Container}>
+        <div className={styles.topCont}>
+          {/* <img className={styles.coverImg} src={Cover}></img>s */}
+          <div className={styles.coverTextCont}>
+            <span className={styles.coverTitle}>Connect on Invader</span>
+            <p className={styles.coverText}>
+              Discover, stream, and share a constantly expanding mix of music
+              from emerging and major artists around the world.
+            </p>
+            {/* <Button className={styles.coverButton}>Sign up for free</Button> */}
+          </div>
+        </div>
+
+        <div className={styles.contentContainer}>
+          <div className={styles.title}>Playlists</div>
+          <div className={styles.playlistContainer}>
+            {data.map((playlist, index) => {
+              return (
+                <Link to="/songs">
+                  <PlayList
+                    key={index + playlist}
+                    image={playlist.userId.image}
+                    title={playlist.name}
+                    Vote={playlist.totalVote}
+                  />
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className={styles.contentContainer}>
-        <div className={styles.title}>Playlists</div>
-        <div className={styles.playlistContainer}>
-          {data.map((playlist, index) => {
-            return (
-              <Link to="/songs">
-                <PlayList
-                  key={index + playlist}
-                  image={playlist.userId.image}
-                  title={playlist.name}
-                  Vote={playlist.totalVote}
-                />
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+      <CreateList />
+    </>
   );
 };
