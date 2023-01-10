e923060049703ec8553b21a55926cfc30ae4ceda
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2023-01-10 13:07:23 +0800

    hi

--- MyMusic/Playlist/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { playlistRoutes } = require("./playlistRoutes");
-const { songRoutes } = require("./songRoutes");
-const { userRoutes } = require("./userRoutes");
-const { artistRoutes } = require("./artistRoutes");
-
-
-module.exports = { playlistRoutes, songRoutes, userRoutes, artistRoutes}
---
+++ tiny/src/components/Pages/Home.jsx
@@ -0,0 +1,5 @@
+import styles from "../assets/home.module.css";
+
+export const Home = () => {
+  return <div className={styles.Container}>Home</div>;
+};
